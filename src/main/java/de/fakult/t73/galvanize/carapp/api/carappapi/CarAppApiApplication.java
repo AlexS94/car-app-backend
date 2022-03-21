039bcdb7bd41daec35fe99d03c219ee12cf5b625
alexS94 <alexS94@development.com>
2022-03-21 13:00:46 +0100

    initialize

---
+++ src/main/java/de/fakult/t73/galvanize/carapp/api/carappapi/CarAppApiApplication.java
@@ -0,0 +1,13 @@
+package de.fakult.t73.galvanize.carapp.api.carappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fakult/t73/galvanize/carapp/api/carappapi/CarAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fakult.t73.galvanize.carapp.api.carappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
